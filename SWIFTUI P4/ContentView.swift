b297d5443414cc582066ff887a8b26adc2380fca
Mohammed Alarbash <mohammed@arbash.net>
2020-05-07 21:02:50 +0300

    Initial Commit

---
+++ SWIFTUI P4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SWIFTUI P4
+//
+//  Created by MOHD ALARBASH on 5/7/20.
+//  Copyright © 2020 MOHD ALARBASH. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
